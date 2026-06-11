df8cba7e2039f9190169492513526f67067a7375
Aditya Kulange <adityakulange1717@gmail.com>
2026-06-11 16:32:00 +0530

    Add files via upload

--- Movie Recommender System/app.py
+++ Movie Recommender System/app.py
@@ -1,1 +1,68 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
 
+
+def fetch_poster(movie_id):
+    try:
+        response = requests.get(
+            f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=6489351b30163c8a9c57eb0f8fae74fa&language=en-US",
+            timeout=10
+        )
+
+        data = response.json()
+
+        poster_path = data.get("poster_path")
+
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+
+        return None
+
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+        # fetch poster from API
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+'Select the movie?',
+movies['title'].values
+)
+
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    cols = [col1, col2, col3, col4, col5]
+
+    for i in range(min(5, len(names))):
+        with cols[i]:
+            st.write(names[i])
+            if i < len(posters) and posters[i]:
+                st.image(posters[i])
